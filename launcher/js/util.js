126181398a0a4847fa60c8a12f97a0405fea7d0e
gwct <greggwct@gmail.com>
2021-05-17 20:56:22 -0600

    added ma weather

--- launcher/js/util.js
+++ launcher/js/util.js
@@ -159,6 +159,22 @@ function getWeatherIN()
     // v2.0 settings
 }
 
+function getWeatherMA()
+// Writes the weather widget.
+{
+    document.write("<a class='weatherwidget-io' href='https://forecast7.com/en/42d39n71d10/somerville/?unit=us' data-label_1='' data-icons='Climacons Animated' data-textcolor='#ffffff' data-font='Arimo'>BOSTON WEATHER</a>");
+    !function(d,s,id){
+        var js,fjs=d.getElementsByTagName(s)[0];
+        if(!d.getElementById(id)){
+            js=d.createElement(s);
+            js.id=id;
+            js.src='https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }
+    (document,'script','weatherwidget-io-js');
+}
+
 ///////////////////////////////////////////////////////
 
 function iotd()
